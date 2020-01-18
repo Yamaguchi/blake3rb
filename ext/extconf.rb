d7d7914729820c9c6c4a99bfa35817be7b9296dd
Hajime Yamaguchi <gen.yamaguchi0@gmail.com>
2020-01-18 19:32:37 +0900

    Update extconf.rb

---
+++ ext/extconf.rb
@@ -0,0 +1,24 @@
+RAKEFILE = 'native.rake'
+
+def command_exists(command, *extra_test_arguments)
+  raise "Command #{command} not found. exitting..." unless system(command, *extra_test_arguments)
+end
+
+command_exists('cargo', '--version')
+command_exists('rustc', '--version')
+
+File.open('Makefile', 'w') do |makefile|
+  makefile.puts <<~MAKEFILE
+    clean:
+    \trake -f #{RAKEFILE} clobber
+    install:
+    \trake -f #{RAKEFILE} build
+  MAKEFILE
+end
+
+File.open(RAKEFILE, 'w') do |rakefile|
+  rakefile.puts <<~RUBY
+    require 'helix_runtime/build_task'
+    HelixRuntime::BuildTask.new
+  RUBY
+end
