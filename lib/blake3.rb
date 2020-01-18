99ca8bc5c78059a902f2d7e9b36b7463e7c8f680
Hajime Yamaguchi <gen.yamaguchi0@gmail.com>
2020-01-18 16:06:30 +0900

    Rename Rust package name

--- lib/blake3.rb
+++ lib/blake3.rb
@@ -1,11 +1,11 @@
 require 'helix_runtime'
 
-require 'blake3/native'
+require 'blake3native/native'
 require "blake3/version"
 
 module Blake3
   module_function
-  
+
   def hexdigest(plain)
     hex = plain.unpack1("H*")
     Blake3Internal.new.from_hex(hex)
@@ -14,5 +14,20 @@ module Blake3
   def digest(plain)
     hex = hexdigest(plain)
     [hex].pack("H*")
+  end
+
+  class Hasher
+    def initialize
+      @hasher = Blake3Hasher.new
+    end
+
+    def update(plain)
+      hex = plain.unpack1("H*")
+      @hasher.update(hex)
+    end
+
+    def finalize
+      @hasher.finalize
+    end
   end
 end
