f4bb6b2e57d749836ce90d22c86ce8243cc91cd0
Hajime Yamaguchi <gen.yamaguchi0@gmail.com>
2020-01-20 14:15:38 +0900

    Add rspec for test vector from https://github.com/BLAKE3-team/BLAKE3/blob/master/test_vectors/test_vectors.json

---
+++ spec/test_vectors_spec.rb
@@ -0,0 +1,20 @@
+require 'json'
+
+# The input in each case is filled with a 251-byte-long repeating pattern:
+# 0, 1, 2, ..., 249, 250, 0, 1, ...
+def create_input(len)
+  (0...len).map { |i| i % 251 }.pack("C*")
+end
+
+RSpec.describe 'TestVectors' do
+  path = "#{File.dirname(__FILE__)}/test_vectors.json"
+  pp json = JSON.parse(File.read(path), symbolize_names: true)
+
+  key = json[:key]
+
+  json[:cases].each do |testcase|
+    input = create_input(testcase[:input_len])
+    it { expect(Blake3.hexdigest(input)).to eq testcase[:hash][0...64] }
+    it { expect(Blake3.hexdigest(input, key: key)).to eq testcase[:keyed_hash][0...64] }
+  end
+end
